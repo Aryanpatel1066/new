c0fde8fb3518eb694c0886c8e49c4407471b69df
aryanPatel <aryanpatel1248@gmail.com>
2024-07-31 21:55:56 +0530

    successfully design nav bar and add routes facility

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
-import { faCartShopping} from "@fortawesome/free-solid-svg-icons";
-import {faUser} from "@fortawesome/free-solid-svg-icons"
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import "../component/Header.css";
 
 export default function Header() {
@@ -12,20 +12,24 @@ export default function Header() {
     <>
       <nav>
         <div className="navLogo">
-          <NavLink to="/" className="navLinks">StyleSavvy</NavLink>
+          <NavLink to="/" className="navLinks">
+            StyleSavvy
+          </NavLink>
         </div>
         <div className="leftCournerLink">
           <div className="inputBox">
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
-        <input />
-        </div>
-          <NavLink className="navLinks" to="/cart"><FontAwesomeIcon icon={faCartShopping}/></NavLink>
-          {" "}
-          <NavLink className="navLinks" to="/products"><FontAwesomeIcon icon={faBagShopping} /></NavLink>
-          {" "}
-          <NavLink className="navLinks" to="/login"><FontAwesomeIcon icon={faUser}/></NavLink>
-          {" "}
-        
+            <FontAwesomeIcon className="searchIcon" icon={faMagnifyingGlass} />
+            <input className="searchBar" placeholder="search on StyleStavvy" />
+          </div>
+          <NavLink className="navLinks" to="/cart">
+            <FontAwesomeIcon icon={faCartShopping} />
+          </NavLink>{" "}
+          <NavLink className="navLinks" to="/products">
+            <FontAwesomeIcon icon={faBagShopping} />
+          </NavLink>{" "}
+          <NavLink className="navLinks" to="/login">
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>{" "}
         </div>
       </nav>
     </>
