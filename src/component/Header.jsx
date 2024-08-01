8bba6a5f1d69de014b7fd962239cff04dd0d4778
aryanPatel <aryanpatel1248@gmail.com>
2024-08-01 22:00:03 +0530

    working on top nav bar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,37 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import "../component/Header.css";
-
+import Navbar from "../pages/NavBar";
+import OfferBar from "../pages/OfferBar";
 export default function Header() {
   return (
     <>
-      <nav>
-        <div className="navLogo">
-          <NavLink to="/" className="navLinks">
-            StyleSavvy
-          </NavLink>
-        </div>
-        <div className="leftCournerLink">
-          <div className="inputBox">
-            <FontAwesomeIcon className="searchIcon" icon={faMagnifyingGlass} />
-            <input className="searchBar" placeholder="search on StyleStavvy" />
-          </div>
-          <NavLink className="navLinks" to="/cart">
-            <FontAwesomeIcon icon={faCartShopping} />
-          </NavLink>{" "}
-          <NavLink className="navLinks" to="/products">
-            <FontAwesomeIcon icon={faBagShopping} />
-          </NavLink>{" "}
-          <NavLink className="navLinks" to="/login">
-            <FontAwesomeIcon icon={faUser} />
-          </NavLink>{" "}
-        </div>
-      </nav>
+      <header>
+        <OfferBar/>
+        <nav>
+          <Navbar />
+          </nav>
+      </header>
     </>
   );
 }
