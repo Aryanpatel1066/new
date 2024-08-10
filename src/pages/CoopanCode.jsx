63579c4db45e83283a654fdbe05aae64d6f823f2
aryanPatel <aryanpatel1248@gmail.com>
2024-08-10 17:40:33 +0530

    complete the offre bar

--- src/pages/CoopanCode.jsx
+++ src/pages/CoopanCode.jsx
@@ -1,20 +1,20 @@
-import "./CoopanCode.css"
-export default function InfoProduct(){
-    return(
-        <>
-        <div className="infoProductContainer">
-          <h2 className="infoHeading">
-            First Purchase Offer
-          </h2>
+import "./CoopanCode.css";
+export default function InfoProduct() {
+  return (
+    <>
+      <div className="infoProductContainer">
+        <h2 className="infoHeading">First Purchase Offer</h2>
+        <div className="parentInfoContainer">
           <div className="twoSubCoopanContainer">
-             <p className="offerText">FREE SHIPPING ON YOUR FIRST PURCHASE</p>
-             <button>USE CODE: FIRSTGLAM</button>
+            <p className="offerText">FREE SHIPPING ON YOUR FIRST PURCHASE</p>
+            <button className="offerButton">USE CODE: FIRSTGLAM</button>
           </div>
           <div className="twoSubCoopanContainer">
-          <p className="offerText">FLAT ₹200 OFF ON ORDERS ABOVE ₹1000</p>
-          <button>USE CODE: FIRST200</button>
+            <p className="offerText">FLAT ₹200 OFF ON ORDERS ABOVE ₹1000</p>
+            <button className="offerButton">USE CODE: FIRST200</button>
           </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
