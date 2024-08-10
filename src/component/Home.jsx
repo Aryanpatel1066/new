2b6d1cb10f35b398d69b232d833cd84f93a99b49
aryanPatel <aryanpatel1248@gmail.com>
2024-08-10 19:20:50 +0530

    done social media icon

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,64 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTruck, faCloudArrowDown, faTag, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons"; // Import from 'free-brands-svg-icons'
+
 import HeroSection from "../pages/HeroSection";
 import CoopanCode from "../pages/CoopanCode";
-  export default function Home(){
-    return(
-        <>
-         {/* <h1>home component</h1> */}
-       <HeroSection/>  
-       <CoopanCode/>
-          </>
-    )
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <CoopanCode />
+
+      {/* Newsletter Section */}
+      <div className="newsLetterContainer">
+        <h2>Newsletter</h2>
+        <p>Get timely updates from your favorite products.</p>
+        <div className="inputContainer">
+          <input placeholder="Your email" />
+          <button>Send</button>
+        </div>
+      </div>
+
+      {/* Facilities Section */}
+      <div className="facilitiesContainer">
+        <div className="topBrandIcon">
+          <div className="facilitiesIcon">
+            <FontAwesomeIcon icon={faTruck} />
+            <p>FREE SHIPPING</p>
+          </div>
+          <div className="facilitiesIcon">
+            <FontAwesomeIcon icon={faTag} />
+            <p>TOP BRANDS</p>
+          </div>
+          <div className="facilitiesIcon">
+            <FontAwesomeIcon icon={faCloudArrowDown} />
+            <p>100% ORIGINAL PRODUCTS</p>
+          </div>
+          <div className="facilitiesIcon">
+            <FontAwesomeIcon icon={faRotateLeft} />
+            <p>EASY RETURNS</p>
+          </div>
+        </div>
+
+        {/* Social Media Section */}
+        <div className="socialMediaIcon">
+          <p>Show some love on social media</p>
+          <div className="socialIconContainer">
+            <div className="socialIcon">
+              <FontAwesomeIcon icon={faGithub} />
+            </div>
+            <div className="socialIcon">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </div>
+            <div className="socialIcon">
+              <FontAwesomeIcon icon={faInstagram} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
